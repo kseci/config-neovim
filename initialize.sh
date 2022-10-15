1cafd8f166c6acb9496ce3b4d2b51470b7615fa9
Dan Filip Solberg <dan.filip.solberg@entur.org>
2022-10-15 12:10:52 +0200

    added idiotic asdf crap

--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #
 #
-echo "Installing pyright globally...."
-sudo npm i -g pyright
+# echo "Installing pyright globally...."
+# sudo npm i -g pyright
+echo "Dowloading packer....."
+git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
