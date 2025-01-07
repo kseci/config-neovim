c6b40a74f01b600794143f0aa0e2b4e0e05a57d2
Dan Filip Solberg <danfilipsolberg@Dans-MacBook-Pro.local>
2025-01-07 01:50:46 +0100

    Small fixes

--- lua/dan/plugins-setup.lua
+++ lua/dan/plugins-setup.lua
@@ -49,7 +49,6 @@ require('packer').startup(function(use)
     requires = {
       'nvim-tree/nvim-web-devicons', -- optional, for file icons
     },
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
   }
   -- Github copilot
   use 'github/copilot.vim'
